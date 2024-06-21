0e13f1e7f98e4098b3ec1bfb1406e38410d8cc1d
Kelton <kelton.tinho23@gmail.com>
2024-06-21 13:22:35 -0300

    react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,16 @@
+import "./Home.css"
+
+
+function Home() {
+
+    return (
+        <>
+            <div id="tela">
+                <div id="home">
+                    <h1>Home</h1></div>
+                <img src="https://i.ibb.co/G91RZf6/Captura-de-tela-2024-06-21-130444.png" alt="" /></div>
+        </>
+    )
+}
+
+export default Home
